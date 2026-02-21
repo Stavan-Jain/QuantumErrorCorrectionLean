da920e29648ed6fc6693294a00ff9b91e30509ab
Stavan Jain <stavansjain@gmail.com>
2026-02-21 13:26:34 -0600

    minor: adds import

--- QEC/Stabilizer/Core.lean
+++ QEC/Stabilizer/Core.lean
@@ -2,3 +2,4 @@ import QEC.Stabilizer.Core.StabilizerGroup
 import QEC.Stabilizer.Core.SubgroupLemmas
 import QEC.Stabilizer.Core.CSS
 import QEC.Stabilizer.Core.CSSNoNegI
+import QEC.Stabilizer.Core.StabilizerCode
